cadc4430f8b8c099d764746b87076054527f88d5
Marat <zhumataev.marat@bk.ru>
2023-09-30 14:20:07 +0500

    add Task 28

---
+++ Task_28/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+int PrNumber(int number)
+{
+    int count = 1;
+    for (int i = 1; i <= number; i++)
+    checked
+    {
+        count *= i;
+    }
+    
+    return count;
+}
+
+Console.Write("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 1)
+{
+    Console.WriteLine("Некорректный ввод!");
+    return;
+} 
+
+int sumNumbers = PrNumber(num);
+
+Console.WriteLine($"{num} -> {sumNumbers}");
